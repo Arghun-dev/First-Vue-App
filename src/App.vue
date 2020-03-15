5c59933611d02938e1ed796f7fcdcdb21fe74faf
Arghun-dev <arghun.developer@gmail.com>
2020-03-15 12:58:41 +0330

    Created My first Vue App

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      title: 'My first Vue App'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Baloo 2', cursive;
+}
+
+h1 {
+  color: #444;
+  text-align: center;
+}
+</style>
