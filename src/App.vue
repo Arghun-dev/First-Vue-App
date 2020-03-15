3deecc9bbefb549fef2be6ee0ecff540abd9ed2c
Arghun-dev <arghun.developer@gmail.com>
2020-03-15 16:02:29 +0330

    Passing data with props

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,34 @@
     <h1>{{ title }}</h1>
     <h2>Navbar Component</h2>
     <Navbar />
+    <h1>All friends</h1>
+    <AllFriends :friends='friends' />
+    <h1>Online friends</h1>
+    <OnlineFriends :friends='friends' />
   </div>
 </template>
 
 <script>
 import Navbar from './Navbar';
+import AllFriends from './AllFriends';
+import OnlineFriends from './OnlineFriends';
 
 export default {
   name: 'app',
   components: {
-    Navbar
+    Navbar,
+    AllFriends,
+    OnlineFriends
   },
   data () {
     return {
-      title: 'My first Vue App'
+      title: 'My first Vue App',
+      friends: [
+        {name: 'Arghun', online: true},
+        {name: 'mammad', online: false},
+        {name: 'milad', online: true},
+        {name: 'Feyzi', online: false}
+      ]
     }
   }
 }
